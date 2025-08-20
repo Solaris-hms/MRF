400acc0b37d9e33922cdd3f2837c22f734bcd8be
palkesh sahu <palkeshsahu02@gmail.com>
2025-08-20 11:41:57 +0530

    idk

--- Backend/config/permissions.go
+++ Backend/config/permissions.go
@@ -1,3 +1,5 @@
+// Backend/config/permissions.go
+
 package config
 
 // AllPermissions defines the complete list of permissions used in the application.
@@ -5,6 +7,8 @@ package config
 var AllPermissions = []string{
 	// User Management
 	"manage:users",
+	"approve:users", // <-- ADD THIS
+	"manage:roles",  // <-- ADD THIS
 
 	// Dashboard
 	"view:dashboard",
@@ -16,9 +20,11 @@ var AllPermissions = []string{
 	"create:inward_entry",
 	"view:inward_entries",
 	"complete:inward_entry",
+	"log:inbound_material", // <-- ADD THIS
 
 	// Sorting
 	"create:sorting_log",
+	"log:sorted_bale", // <-- ADD THIS
 
 	// Assets
 	"view:assets",
@@ -26,5 +32,6 @@ var AllPermissions = []string{
 	"edit:assets",
 	"delete:assets",
 
-	// Add any new permissions here in the future
+	// Reports (example, add if you need it)
+	"generate:reports", // <-- ADD THIS
 }
