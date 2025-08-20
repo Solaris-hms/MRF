f4f3c2892a826ead7fb9be237c62f6b4841edb49
palkesh sahu <palkeshsahu02@gmail.com>
2025-08-20 12:27:19 +0530

    Forced redeploy to apply latest secrets

--- Backend/main.go
+++ Backend/main.go
@@ -14,7 +14,7 @@ import (
 
 func main() {
 	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found")
+		log.Println("No .env file found.")
 	}
 	db := database.New()
 	defer db.Close()
